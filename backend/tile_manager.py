17d33e1d776979fc28b46baf33c1ac4ef81572e9
acobb <aecobb53@gmail.com>
2020-12-14 11:04:26 -0700

    getter setter added to tile manager

--- backend/tile_manager.py
+++ backend/tile_manager.py
@@ -22,147 +22,56 @@ class TileManager:
         The values of the kwargs are converted to lists if they are not already then provided to the Tile class.
         """
         
-        self.cc = None
-        self.i0 = None
-        self.i1 = None
-        self.i2 = None
-        self.i3 = None
-        self.i4 = None
-        self.i5 = None
-        self.m0 = None
-        self.m1 = None
-        self.m2 = None
-        self.m3 = None
-        self.m4 = None
-        self.m5 = None
-        self.m6 = None
-        self.m7 = None
-        self.m8 = None
-        self.m9 = None
-        self.m10 = None
-        self.m11 = None
-        self.o0 = None
-        self.o1 = None
-        self.o2 = None
-        self.o3 = None
-        self.o4 = None
-        self.o5 = None
-        self.o6 = None
-        self.o7 = None
-        self.o8 = None
-        self.o9 = None
-        self.o10 = None
-        self.o11 = None
-        self.o12 = None
-        self.o13 = None
-        self.o14 = None
-        self.o15 = None
-        self.o16 = None
-        self.o17 = None
-
-        self.erah = None
-        self.govener = None
-        self.amenities = None
-        self.power = None
-        self.bonus = None
-        self.strategic = None
-        self.luxury = None
-        self.trader = None
+        self._cc = None
+        self._i0 = None
+        self._i1 = None
+        self._i2 = None
+        self._i3 = None
+        self._i4 = None
+        self._i5 = None
+        self._m0 = None
+        self._m1 = None
+        self._m2 = None
+        self._m3 = None
+        self._m4 = None
+        self._m5 = None
+        self._m6 = None
+        self._m7 = None
+        self._m8 = None
+        self._m9 = None
+        self._m10 = None
+        self._m11 = None
+        self._o0 = None
+        self._o1 = None
+        self._o2 = None
+        self._o3 = None
+        self._o4 = None
+        self._o5 = None
+        self._o6 = None
+        self._o7 = None
+        self._o8 = None
+        self._o9 = None
+        self._o10 = None
+        self._o11 = None
+        self._o12 = None
+        self._o13 = None
+        self._o14 = None
+        self._o15 = None
+        self._o16 = None
+        self._o17 = None
+        self._erah = None
+        self._govener = None
+        self._amenities = None
+        self._power = None
+        self._bonus = None
+        self._strategic = None
+        self._luxury = None
+        self._trader = None
 
         for k,v in kwargs.items():
             if not isinstance(v, list):
                 v = [v]
-            if k == 'cc':
-                self.cc = Tile(v)
-            if k == 'i0':
-                self.i0 = Tile(v)
-            if k == 'i1':
-                self.i1 = Tile(v)
-            if k == 'i2':
-                self.i2 = Tile(v)
-            if k == 'i3':
-                self.i3 = Tile(v)
-            if k == 'i4':
-                self.i4 = Tile(v)
-            if k == 'i5':
-                self.i5 = Tile(v)
-            if k == 'm0':
-                self.m0 = Tile(v)
-            if k == 'm1':
-                self.m1 = Tile(v)
-            if k == 'm2':
-                self.m2 = Tile(v)
-            if k == 'm3':
-                self.m3 = Tile(v)
-            if k == 'm4':
-                self.m4 = Tile(v)
-            if k == 'm5':
-                self.m5 = Tile(v)
-            if k == 'm6':
-                self.m6 = Tile(v)
-            if k == 'm7':
-                self.m7 = Tile(v)
-            if k == 'm8':
-                self.m8 = Tile(v)
-            if k == 'm9':
-                self.m9 = Tile(v)
-            if k == 'm10':
-                self.m10 = Tile(v)
-            if k == 'm11':
-                self.m11 = Tile(v)
-            if k == 'o0':
-                self.o0 = Tile(v)
-            if k == 'o1':
-                self.o1 = Tile(v)
-            if k == 'o2':
-                self.o2 = Tile(v)
-            if k == 'o3':
-                self.o3 = Tile(v)
-            if k == 'o4':
-                self.o4 = Tile(v)
-            if k == 'o5':
-                self.o5 = Tile(v)
-            if k == 'o6':
-                self.o6 = Tile(v)
-            if k == 'o7':
-                self.o7 = Tile(v)
-            if k == 'o8':
-                self.o8 = Tile(v)
-            if k == 'o9':
-                self.o9 = Tile(v)
-            if k == 'o10':
-                self.o10 = Tile(v)
-            if k == 'o11':
-                self.o11 = Tile(v)
-            if k == 'o12':
-                self.o12 = Tile(v)
-            if k == 'o13':
-                self.o13 = Tile(v)
-            if k == 'o14':
-                self.o14 = Tile(v)
-            if k == 'o15':
-                self.o15 = Tile(v)
-            if k == 'o16':
-                self.o16 = Tile(v)
-            if k == 'o17':
-                self.o17 = Tile(v)
-
-            if k == 'erah':
-                self.erah = v[0]
-            if k == 'govener':
-                self.govener = v
-            if k == 'amenities':
-                self.amenities = v
-            if k == 'power':
-                self.power = v
-            if k == 'bonus':
-                self.bonus = v
-            if k == 'strategic':
-                self.strategic = v
-            if k == 'luxury':
-                self.luxury = v
-            if k == 'trader':
-                self.trader = v
+            setattr(self, k, v)
 
         # Era:0 Ancient Era (4000 BC ~ 1000 BC)
         # Era:1 Classical Era (1000 BC ~ 500 AD)
@@ -174,43 +83,539 @@ class TileManager:
         # Era:7 Information Era (1995 ~ 2020)
         # Era:8 Future Era GS-Only.png (2020 ~ 2050)
         # print(json.dumps(self.__dict__, indent=4))
-        print(f"cc: {self.cc}")
-        print(f"i0: {self.i0}")
-        print(f"i1: {self.i1}")
-        print(f"i2: {self.i2}")
-        print(f"i3: {self.i3}")
-        print(f"i4: {self.i4}")
-        print(f"i5: {self.i5}")
-        print(f"m0: {self.m0}")
-        print(f"m1: {self.m1}")
-        print(f"m2: {self.m2}")
-        print(f"m3: {self.m3}")
-        print(f"m4: {self.m4}")
-        print(f"m5: {self.m5}")
-        print(f"m6: {self.m6}")
-        print(f"m7: {self.m7}")
-        print(f"m8: {self.m8}")
-        print(f"m9: {self.m9}")
-        print(f"m10: {self.m10}")
-        print(f"m11: {self.m11}")
-        print(f"o0: {self.o0}")
-        print(f"o1: {self.o1}")
-        print(f"o2: {self.o2}")
-        print(f"o3: {self.o3}")
-        print(f"o4: {self.o4}")
-        print(f"o5: {self.o5}")
-        print(f"o6: {self.o6}")
-        print(f"o7: {self.o7}")
-        print(f"o8: {self.o8}")
-        print(f"o9: {self.o9}")
-        print(f"o10: {self.o10}")
-        print(f"o11: {self.o11}")
-        print(f"o12: {self.o12}")
-        print(f"o13: {self.o13}")
-        print(f"o14: {self.o14}")
-        print(f"o15: {self.o15}")
-        print(f"o16: {self.o16}")
-        print(f"o17: {self.o17}")
+        # print(f"cc: {self.cc}")
+        # print(f"i0: {self.i0}")
+        # print(f"i1: {self.i1}")
+        # print(f"i2: {self.i2}")
+        # print(f"i3: {self.i3}")
+        # print(f"i4: {self.i4}")
+        # print(f"i5: {self.i5}")
+        # print(f"m0: {self.m0}")
+        # print(f"m1: {self.m1}")
+        # print(f"m2: {self.m2}")
+        # print(f"m3: {self.m3}")
+        # print(f"m4: {self.m4}")
+        # print(f"m5: {self.m5}")
+        # print(f"m6: {self.m6}")
+        # print(f"m7: {self.m7}")
+        # print(f"m8: {self.m8}")
+        # print(f"m9: {self.m9}")
+        # print(f"m10: {self.m10}")
+        # print(f"m11: {self.m11}")
+        # print(f"o0: {self.o0}")
+        # print(f"o1: {self.o1}")
+        # print(f"o2: {self.o2}")
+        # print(f"o3: {self.o3}")
+        # print(f"o4: {self.o4}")
+        # print(f"o5: {self.o5}")
+        # print(f"o6: {self.o6}")
+        # print(f"o7: {self.o7}")
+        # print(f"o8: {self.o8}")
+        # print(f"o9: {self.o9}")
+        # print(f"o10: {self.o10}")
+        # print(f"o11: {self.o11}")
+        # print(f"o12: {self.o12}")
+        # print(f"o13: {self.o13}")
+        # print(f"o14: {self.o14}")
+        # print(f"o15: {self.o15}")
+        # print(f"o16: {self.o16}")
+        # print(f"o17: {self.o17}")
+
+    # cc
+    @property
+    def cc(self):
+        if self._cc == None:
+            return None
+        return self._cc
+
+    @cc.setter
+    def cc(self, value):
+        self._cc = Tile(value)
+
+    # i0
+    @property
+    def i0(self):
+        if self._i0 == None:
+            return None
+        return self._i0
+
+    @i0.setter
+    def i0(self, value):
+        self._i0 = Tile(value)
+
+    # i1
+    @property
+    def i1(self):
+        if self._i1 == None:
+            return None
+        return self._i1
+
+    @i1.setter
+    def i1(self, value):
+        self._i1 = Tile(value)
+
+    # i2
+    @property
+    def i2(self):
+        if self._i2 == None:
+            return None
+        return self._i2
+
+    @i2.setter
+    def i2(self, value):
+        self._i2 = Tile(value)
+
+    # i3
+    @property
+    def i3(self):
+        if self._i3 == None:
+            return None
+        return self._i3
+
+    @i3.setter
+    def i3(self, value):
+        self._i3 = Tile(value)
+
+    # i4
+    @property
+    def i4(self):
+        if self._i4 == None:
+            return None
+        return self._i4
+
+    @i4.setter
+    def i4(self, value):
+        self._i4 = Tile(value)
+
+    # i5
+    @property
+    def i5(self):
+        if self._i5 == None:
+            return None
+        return self._i5
+
+    @i5.setter
+    def i5(self, value):
+        self._i5 = Tile(value)
+
+    # m0
+    @property
+    def m0(self):
+        if self._m0 == None:
+            return None
+        return self._m0
+
+    @m0.setter
+    def m0(self, value):
+        self._m0 = Tile(value)
+
+    # m1
+    @property
+    def m1(self):
+        if self._m1 == None:
+            return None
+        return self._m1
+
+    @m1.setter
+    def m1(self, value):
+        self._m1 = Tile(value)
+
+    # m2
+    @property
+    def m2(self):
+        if self._m2 == None:
+            return None
+        return self._m2
+
+    @m2.setter
+    def m2(self, value):
+        self._m2 = Tile(value)
+
+    # m3
+    @property
+    def m3(self):
+        if self._m3 == None:
+            return None
+        return self._m3
+
+    @m3.setter
+    def m3(self, value):
+        self._m3 = Tile(value)
+
+    # m4
+    @property
+    def m4(self):
+        if self._m4 == None:
+            return None
+        return self._m4
+
+    @m4.setter
+    def m4(self, value):
+        self._m4 = Tile(value)
+
+    # m5
+    @property
+    def m5(self):
+        if self._m5 == None:
+            return None
+        return self._m5
+
+    @m5.setter
+    def m5(self, value):
+        self._m5 = Tile(value)
+
+    # m6
+    @property
+    def m6(self):
+        if self._m6 == None:
+            return None
+        return self._m6
+
+    @m6.setter
+    def m6(self, value):
+        self._m6 = Tile(value)
+
+    # m7
+    @property
+    def m7(self):
+        if self._m7 == None:
+            return None
+        return self._m7
+
+    @m7.setter
+    def m7(self, value):
+        self._m7 = Tile(value)
+
+    # m8
+    @property
+    def m8(self):
+        if self._m8 == None:
+            return None
+        return self._m8
+
+    @m8.setter
+    def m8(self, value):
+        self._m8 = Tile(value)
+
+    # m9
+    @property
+    def m9(self):
+        if self._m9 == None:
+            return None
+        return self._m9
+
+    @m9.setter
+    def m9(self, value):
+        self._m9 = Tile(value)
+
+    # m10
+    @property
+    def m10(self):
+        if self._m10 == None:
+            return None
+        return self._m10
+
+    @m10.setter
+    def m10(self, value):
+        self._m10 = Tile(value)
+
+    # m11
+    @property
+    def m11(self):
+        if self._m11 == None:
+            return None
+        return self._m11
+
+    @m11.setter
+    def m11(self, value):
+        self._m11 = Tile(value)
+
+    # o0
+    @property
+    def o0(self):
+        if self._o0 == None:
+            return None
+        return self._o0
+
+    @o0.setter
+    def o0(self, value):
+        self._o0 = Tile(value)
+
+    # o1
+    @property
+    def o1(self):
+        if self._o1 == None:
+            return None
+        return self._o1
+
+    @o1.setter
+    def o1(self, value):
+        self._o1 = Tile(value)
+
+    # o2
+    @property
+    def o2(self):
+        if self._o2 == None:
+            return None
+        return self._o2
+
+    @o2.setter
+    def o2(self, value):
+        self._o2 = Tile(value)
+
+    # o3
+    @property
+    def o3(self):
+        if self._o3 == None:
+            return None
+        return self._o3
+
+    @o3.setter
+    def o3(self, value):
+        self._o3 = Tile(value)
+
+    # o4
+    @property
+    def o4(self):
+        if self._o4 == None:
+            return None
+        return self._o4
+
+    @o4.setter
+    def o4(self, value):
+        self._o4 = Tile(value)
+
+    # o5
+    @property
+    def o5(self):
+        if self._o5 == None:
+            return None
+        return self._o5
+
+    @o5.setter
+    def o5(self, value):
+        self._o5 = Tile(value)
+
+    # o6
+    @property
+    def o6(self):
+        if self._o6 == None:
+            return None
+        return self._o6
+
+    @o6.setter
+    def o6(self, value):
+        self._o6 = Tile(value)
+
+    # o7
+    @property
+    def o7(self):
+        if self._o7 == None:
+            return None
+        return self._o7
+
+    @o7.setter
+    def o7(self, value):
+        self._o7 = Tile(value)
+
+    # o8
+    @property
+    def o8(self):
+        if self._o8 == None:
+            return None
+        return self._o8
+
+    @o8.setter
+    def o8(self, value):
+        self._o8 = Tile(value)
+
+    # o9
+    @property
+    def o9(self):
+        if self._o9 == None:
+            return None
+        return self._o9
+
+    @o9.setter
+    def o9(self, value):
+        self._o9 = Tile(value)
+
+    # o10
+    @property
+    def o10(self):
+        if self._o10 == None:
+            return None
+        return self._o10
+
+    @o10.setter
+    def o10(self, value):
+        self._o10 = Tile(value)
+
+    # o11
+    @property
+    def o11(self):
+        if self._o11 == None:
+            return None
+        return self._o11
+
+    @o11.setter
+    def o11(self, value):
+        self._o11 = Tile(value)
+
+    # o12
+    @property
+    def o12(self):
+        if self._o12 == None:
+            return None
+        return self._o12
+
+    @o12.setter
+    def o12(self, value):
+        self._o12 = Tile(value)
+
+    # o13
+    @property
+    def o13(self):
+        if self._o13 == None:
+            return None
+        return self._o13
+
+    @o13.setter
+    def o13(self, value):
+        self._o13 = Tile(value)
+
+    # o14
+    @property
+    def o14(self):
+        if self._o14 == None:
+            return None
+        return self._o14
+
+    @o14.setter
+    def o14(self, value):
+        self._o14 = Tile(value)
+
+    # o15
+    @property
+    def o15(self):
+        if self._o15 == None:
+            return None
+        return self._o15
+
+    @o15.setter
+    def o15(self, value):
+        self._o15 = Tile(value)
+
+    # o16
+    @property
+    def o16(self):
+        if self._o16 == None:
+            return None
+        return self._o16
+
+    @o16.setter
+    def o16(self, value):
+        self._o16 = Tile(value)
+
+    # o17
+    @property
+    def o17(self):
+        if self._o17 == None:
+            return None
+        return self._o17
+
+    @o17.setter
+    def o17(self, value):
+        self._o17 = Tile(value)
+
+    # erah
+    @property
+    def erah(self):
+        if self._erah == None:
+            return None
+        return self._erah
+
+    @erah.setter
+    def erah(self, value):
+        self._erah = Tile(value)
+
+    # govener
+    @property
+    def govener(self):
+        if self._govener == None:
+            return None
+        return self._govener
+
+    @govener.setter
+    def govener(self, value):
+        self._govener = Tile(value)
+
+    # amenities
+    @property
+    def amenities(self):
+        if self._amenities == None:
+            return None
+        return self._amenities
+
+    @amenities.setter
+    def amenities(self, value):
+        self._amenities = Tile(value)
+
+    # power
+    @property
+    def power(self):
+        if self._power == None:
+            return None
+        return self._power
+
+    @power.setter
+    def power(self, value):
+        self._power = Tile(value)
+
+    # bonus
+    @property
+    def bonus(self):
+        if self._bonus == None:
+            return None
+        return self._bonus
+
+    @bonus.setter
+    def bonus(self, value):
+        self._bonus = Tile(value)
+
+    # strategic
+    @property
+    def strategic(self):
+        if self._strategic == None:
+            return None
+        return self._strategic
+
+    @strategic.setter
+    def strategic(self, value):
+        self._strategic = Tile(value)
+
+    # luxury
+    @property
+    def luxury(self):
+        if self._luxury == None:
+            return None
+        return self._luxury
+
+    @luxury.setter
+    def luxury(self, value):
+        self._luxury = Tile(value)
+
+    # trader
+    @property
+    def trader(self):
+        if self._trader == None:
+            return None
+        return self._trader
+
+    @trader.setter
+    def trader(self, value):
+        self._trader = Tile(value)
+
 
     def calculate_adjacency(self, tile_index):
         """
---
+++ docs/comparing_tiles.md
@@ -0,0 +1,230 @@
+# Breakdown of tiles and stuff
+
+Really this is for notes to keep track of for building things out
+
+# Terrains
+- grassland
+- plains
+- desert
+- tundra
+- snow
+- coast
+    - tiles next to land provide 1 housing
+- lake
+    - provides fresh water housing bonus
+- ocean
+    - provides 1 appeal to adjacent tiles
+
+# Features
+- woods
+    - only created on `grassland`, `grasslandH`, `plains`, `plainsH`, `tundra`, `tundraH`
+    - 1 appeal to adjacent tiles
+- rainforest
+    - only created on `plains`, `plainsH`
+    - -1 appeal to surrounding tiles
+- marsh
+    - only on `grassland`
+    - -1 appeal to surrounding tiles
+- floodplains
+    - only on `desert`, `plains`, `grassland`
+    - provides fresh water housing bonus
+- oasis
+    - only on `desert`
+    - 1 appeal to adj tiles
+    - provides fresh water
+- cliffs
+- reef
+- ice
+- cataract
+- volcano
+- volcanic_soil
+- geothermal
+- impact_zone
+- mountains
+- river
+    - 1 appeal to adjacent tiles
+    - provides fresh water housing bonus
+
+# Improvements
+
+## Standard buildings
+- farm
+    - some housing
+    - extra food based on additional farms and the era
+- min
+    - less appeal
+    - extra production based on era
+- quarry
+    - less appeal
+    - extra production with era
+- plantation
+    - some housing
+    - extra food/gold based on era
+- camp
+    - some housing
+- pasture
+    - 
+- fishing_boat
+    - 
+- lumber_mill
+    - 
+- fort
+    - 
+- airstrip
+    - 
+- seaside_resprot
+    - 
+- geothermal_plant
+    - 
+- wind_farm
+    - 
+- solar_farm
+    - 
+- offshore_wind_farm
+    - 
+- ski_resort
+    - 
+- oil_well
+    - 
+- offshore_oil_well
+    - 
+- missle_silo
+    - 
+- mountain_tunnel
+    - 
+- railroad
+    - 
+- seastead
+    - 
+
+## City state buildings
+- alcazar
+    - 
+- batey
+    - 
+- cahokia_mounds
+    - 
+- colossal_heads
+    - 
+- mahavihara
+    - 
+- moai
+    - 
+- monistary
+    - 
+- nazca_line
+    - 
+- trading_dome
+    - 
+
+## Unique buildings
+- chateau
+    - 
+- chemamull
+    - 
+- golf_course
+    - 
+- great_wall
+    - 
+- hacienda
+    - 
+- ice_hocky_rink
+    - 
+- kampung
+    - 
+- kurgan
+    - 
+- mekewap
+    - 
+- mission
+    - 
+- nubian_pyramid
+    - 
+- open-air_museum
+    - 
+- outback_station
+    - 
+- pa
+    - 
+- pairirdaeza
+    - 
+- polder
+    - 
+- qhapaq_nan
+    - 
+- rock-hewn_church
+    - 
+- roman_fort
+    - 
+- sphinx
+    - 
+- stepwell
+    - 
+- terrace_farms
+    - 
+- ziggurat
+    - 
+
+## Governor buildings
+- city_park
+    - 
+- fishery
+    - 
+
+
+# Districts
+- city_center
+    - 
+- campus
+    - 
+- theater_square
+    - 
+- holy_site
+    - 
+- encampment
+    - 
+- commercial_hub
+    - 
+- harbor
+    - 
+- industrial_zone
+    - 
+- entertainment_complex
+    - 
+- water_park
+    - 
+- aqueduct
+    - 
+- neighborhood
+    - 
+- canal
+    - 
+- dam
+    - 
+- aerodrome
+    - 
+- spaceport
+    - 
+- govermment_plaza
+    - 
+- diplomatic_quarter
+    - 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
