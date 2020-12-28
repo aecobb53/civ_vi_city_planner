1e76a8f5833246603c1844dc3a58bd7d0cbe84e0
Domenic Murtari <domenic.murtari@gmail.com>
2020-12-28 14:08:22 -0700

    Regenerate with axios client

--- bin/generate_clients.sh
+++ bin/generate_clients.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 openapi-generator generate -i swagger.yaml -o client \
--g typescript-fetch \
--p typescriptThreePlus=true,prefixParameterInterfaces=true
+-g typescript-axios \
+-p typescriptThreePlus=true,prefixParameterInterfaces=true,supportsES6=true
